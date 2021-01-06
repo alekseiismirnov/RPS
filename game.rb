4f0be95df1c9be8be1885b7b5c447e1454ae78e8
Alekseii Smirnov <alekseii.sm@gmail.com>
2021-01-06 18:48:50 +0200

    game, tested manualy due to user input

---
+++ game.rb
@@ -0,0 +1,28 @@
+$LOAD_PATH.unshift("#{File.dirname(__FILE__)}/lib")
+
+require 'rps'
+
+class Game
+  def result(first_result, second_result)
+    return :win if first_result and not second_result
+    return :lose if not first_result and second_result
+    return :tie
+  end
+
+  def round
+    rps_round = RPS.new
+
+    computers_hand = rps_round.possible_hands.sample
+    puts "Your move:"
+    players_hand = gets.chomp
+
+    puts "Computer says: \"#{computers_hand}\""
+
+    puts result(rps_round.wins?(players_hand, computers_hand), 
+                rps_round.wins?(computers_hand, players_hand))
+  end
+end
+
+rps_game = Game.new
+puts rps_game.round
+
