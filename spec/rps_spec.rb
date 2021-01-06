3c5fc27409b8ca3dd526388fd24758c10b4a4cde
Alekseii Smirnov <alekseii.sm@gmail.com>
2021-01-06 14:42:57 +0200

    tested for all wins

--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -17,4 +17,8 @@ describe RPS do
     expect(game.wins?("scissors", "paper")).to(eq(true))
   end
 
+  it("returns true if paper is the object and rock is the argument") do
+    game = RPS.new()
+    expect(game.wins?("paper", "rock")).to(eq(true))
+  end
 end
